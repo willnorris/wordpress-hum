413f45813f8d5c1491505e89c063e6a5d682be54
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-06-27 09:39:08 +0200

    version bump

--- hum.php
+++ hum.php
@@ -5,7 +5,7 @@
  * Description: Personal URL shortener for WordPress
  * Author: Will Norris
  * Author URI: https://willnorris.com/
- * Version: 1.3.2
+ * Version: 1.3.3
  * License: MIT
  * License URI: http://opensource.org/licenses/MIT
  * Text Domain: hum
